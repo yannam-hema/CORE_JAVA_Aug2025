9f387f57e478fa3bee1709b3f8f8dc18e8416a07
yannam-hema <yannamhema30@gmail.com>
2025-08-10 13:47:01 +0530

    Intial Commit

---
+++ CORE_JAVA_NEW/src/com/gqt/common/FirstOne.java
@@ -0,0 +1,10 @@
+package com.gqt.common;
+
+public class FirstOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Hi!!! Hema");
+	}
+
+}
